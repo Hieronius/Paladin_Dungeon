205b960c45eb42b9d2f2e114751b41217d228004
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-09 17:17:38 +0300

    Add method to iterate array of characters

---
+++ Paladin_Dungeon/Paladin_Dungeon/Access to array of characters.c
@@ -0,0 +1,25 @@
+//
+//  Access to array of characters.c
+//  Paladin_Dungeon
+//
+//  Created by Арсентий Халимовский on 08.01.2024.
+//
+
+#include <stdio.h>
+
+//int main() {
+//    character paladin = {"Hieronius", 10, 5, 100, 50, 80, 2, 100};
+//    character goblin = {"goblin", 5, 0, 20, 0, 50, 1, 100};
+//    character skeleton = {"skeleton", 7, 0, 25, 0, 55, 1, 100};
+//    character ghoul = {"ghoul", 8, 0, 30, 0, 60, 1, 100};
+//    character wolf = {"mad wolf", 9, 0, 35, 0, 65, 1, 100};
+//    character ork = {"ork", 10, 0, 40, 0, 65, 1, 100};
+//    
+//    character arrayOfCharacters[5] = {goblin, skeleton, ghoul, wolf, ork};
+//    
+//    for (int i = 0; i < 5; i++) {
+//        printf("%s\n", (arrayOfCharacters + i)->name);
+//    }
+//    
+//    return 0;
+//}
