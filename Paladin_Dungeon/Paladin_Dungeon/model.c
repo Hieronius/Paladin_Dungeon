9255f189ab4782482f30331ad271d718f9a81a61
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-09 18:53:39 +0300

    Add file with data types of the objects in game

---
+++ Paladin_Dungeon/Paladin_Dungeon/model.c
@@ -0,0 +1,47 @@
+//
+//  model.c
+//  Paladin_Dungeon
+//
+//  Created by Арсентий Халимовский on 06.01.2024.
+//
+
+#include "model.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//typedef struct {
+//    char name[100];
+//    int attack;
+//    int defence;
+//    int health;
+//    int mana;
+//    int energy;
+//    int mental;
+//} character;
+//
+//typedef struct {
+//    int capacity;
+//    char *inventory;
+//    int weight;
+//} storage;
+//
+//typedef struct {
+//    char *healingSpell;
+//    char *defensiveSpell;
+//    char *attackSpell;
+//    char *meditationSpeel;
+//} spells;
+//
+//typedef struct {
+//    char *attack;
+//    char *block;
+//    char *charge;
+//    char *parry;
+//} skills;
+//
+//typedef struct {
+//    char *open;
+//    char *move;
+//    char *use;
+//} actions;
---
+++ Paladin_Dungeon/Paladin_Dungeon/model.h
@@ -0,0 +1,50 @@
+//
+//  model.h
+//  Paladin_Dungeon
+//
+//  Created by Арсентий Халимовский on 06.01.2024.
+//
+
+#ifndef model_h
+#define model_h
+
+#include <stdio.h>
+
+typedef struct {
+    char name[100];
+    int attack;
+    int defence;
+    int health;
+    int mana;
+    int energy; // cost per hit or spell
+    int stamina; // amount of actions to choose per round
+    int mental;
+} character;
+
+typedef struct {
+    int capacity;
+    char *inventory;
+    int weight;
+} storage;
+
+typedef struct {
+    char *healingSpell;
+    char *defensiveSpell;
+    char *attackSpell;
+    char *meditationSpeel;
+} spells;
+
+typedef struct {
+    char *attack;
+    char *block;
+    char *charge;
+    char *parry;
+} skills;
+
+typedef struct {
+    char *open;
+    char *move;
+    char *use;
+} actions;
+
+#endif /* model_h */
