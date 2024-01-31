81d6a58d398b0e1058790af56103cb74d58ffbe4
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-31 09:53:57 +0300

    Add separate file with font colors

---
+++ Paladin_Dungeon/Paladin_Dungeon/colors.h
@@ -0,0 +1,17 @@
+
+#ifndef colors_h
+#define colors_h
+
+#include <stdio.h>
+
+#define GREEN "\x1b[32m"
+#define BLACK "\x1b[30m"
+#define RED "\x1b[31m"
+#define BLUE "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN "\x1b[36m"
+#define YELLOW "\x1b[33m"
+#define WHITE "\x1b[0m"
+#define SOUND "\a"
+
+#endif /* colors_h */
