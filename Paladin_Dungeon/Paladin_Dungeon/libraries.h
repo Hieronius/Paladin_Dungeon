8a4686f9e637196315b3117873bcd4951df31ada
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-02 03:31:30 +0300

    Add file for all libraries included

---
+++ Paladin_Dungeon/Paladin_Dungeon/libraries.h
@@ -0,0 +1,25 @@
+//
+//  libraries.h
+//  Paladin_Dungeon
+//
+//  Created by Арсентий Халимовский on 31.01.2024.
+//
+
+#ifndef libraries_h
+#define libraries_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include <ncurses.h>
+#include <unistd.h>
+#include <time.h>
+#include <termios.h>
+
+#include "colors.h"
+#include "model.h"
+#include "functions.h"
+
+#endif /* libraries_h */
