635381059cc0640f5c5c5c61c05bbfef1c2bb3d1
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-03 08:05:42 +0300

    Add method prototypes to separate file

---
+++ Paladin_Dungeon/Paladin_Dungeon/functions.h
@@ -0,0 +1,25 @@
+//
+//  functions.h
+//  Paladin_Dungeon
+//
+//  Created by Арсентий Халимовский on 31.01.2024.
+//
+
+#ifndef functions_h
+#define functions_h
+
+void attackEnemy(Hero *attacker, Enemy *target);
+void attackHero(Enemy *attacker, Hero *target);
+void block(Hero *character);
+void heal(Hero *healer);
+void magic(Hero *caster, Enemy *target);
+void useObject(Hero *user); // I need an inventory for it
+void startGame(bool *isAlive);
+void moveToEnemy(char hotKey);
+void getLevel(Hero *hero);
+void enableRawMode(void);
+void disableRawMode(void);
+Enemy getEnemy(Enemy *array);
+Hero getHero(void);
+
+#endif /* functions_h */
